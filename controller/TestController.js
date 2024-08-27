6bdb8073066f2dc0f99a6164afa2c66407778b0b
Yaser Syed <yaser1091@gmail.com>
2024-08-26 22:51:34 -0500

    Initial app configuration

---
+++ controller/TestController.js
@@ -0,0 +1,6 @@
+const ve = require('../VectorEmbeddings')
+
+exports.test = (req, res) => {
+    res.send("executing test VectorEmbeddings request");
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const testController = require('../controller/TestController');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/test', testController.test)
 
 module.exports = router;
