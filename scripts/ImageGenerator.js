f75c5ef33acd4adb77a2a65d83d1eb228619e4b2
Yaser Syed <yaser1091@gmail.com>
2024-10-27 00:34:00 -0500

    save generated images

--- scripts/ImageGenerator.js
+++ scripts/ImageGenerator.js
@@ -1,5 +1,19 @@
+const OpenAI = require('openai');
 const fs = require('fs');
 const axios = require('axios');
+
+var openai = new OpenAI();
+
+// async function sendImageGenRequest(prompt) {
+//     const response = await openai.images.generate({
+//         model: "dall-e-3",
+//         prompt: prompt,
+//         n: 1,
+//         size: "1024x1024",
+//     });
+//     return response.data;
+// }
+
 
 async function sendImageGenRequest(prompt) {
     const response = await openai.images.generate({
@@ -8,14 +22,23 @@ async function sendImageGenRequest(prompt) {
         n: 1,
         size: "1024x1024",
     });
-    const imageUrl = response.data.data[0].url;
+
+    const imageUrl = response && response.data ? response.data[0].url : '';
     const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const buffer = Buffer.from(imageResponse.data, 'binary');
-    res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': buffer.length,
-      });
-    res.end(buffer);
+    var fileName = String(prompt).toLowerCase();
+    fileName = 'generated-images' + '/' + fileName.replaceAll(' ','-') + '.jpg';
+
+    fs.writeFileSync(fileName, imageResponse.data); // Save as "image.jpg"
+    console.log('Image saved as image.jpg');
+
+    // Append the image to the DOM, e.g., a div with id "imageContainer"
+    // document.getElementById('imageContainer').appendChild(imgElement);
+    // const buffer = Buffer.from(imageResponse.data, 'binary');
+    // res.writeHead(200, {
+    //     'Content-Type': 'image/png',
+    //     'Content-Length': buffer.length,
+    //   });
+    // res.end(buffer);
     return response.data;
 }
 
